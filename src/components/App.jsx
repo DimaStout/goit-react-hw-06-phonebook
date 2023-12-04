96ea1a326ac585c766368f653c8a202fe7fa07f2
Dmytro Zinchuk <127327533+DimaStout@users.noreply.github.com>
2023-12-04 14:21:01 +0100

    goit-react-hw-06-phonebook-001

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import s from './App.module.css';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.div}>
+      <h1 className={s.title}>Phonebook</h1>
+      <ContactForm />
+      <h2 className={s.title}>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
+
+export default App;
